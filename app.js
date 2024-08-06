d65612474921e58b30691830142c1c07f61d4a02
Italo Adler <italo.adler@DIGITALCOLLEGE.LOCAL>
2024-08-06 20:46:38 -0300

    Refatora primeira versão

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 import { createServer } from 'http';
-import { readFileSync, writeFile, readFile } from 'fs';
+import { readFileSync, writeFileSync, readFile } from 'fs';
 
 function lerDados() {
     return readFileSync('db.json', 'utf-8');
@@ -9,55 +9,92 @@ function salvarDados(dados) {
     writeFileSync('db.json', JSON.stringify(dados, null, 2));
 }
 
+function handleGetProdutos(req, res) {
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.write(lerDados());
+    res.end();
+}
+
+function handlePostProdutos(req, res) {
+    let body = '';
+    req.on('data', chunk => {
+        body += chunk;
+    });
+    req.on('end', () => {
+        const dados = JSON.parse(body);
+        const produtos = JSON.parse(lerDados());
+        produtos.push(dados);
+        salvarDados(produtos);
+        res.writeHead(201, { 'Content-Type': 'application/json' });
+        res.write(lerDados());
+        res.end();
+    });
+}
+
+function handleGetIndex(req, res) {
+    readFile('index.html', (err, data) => {
+        if (err) {
+            res.writeHead(500, { 'Content-Type': 'text/plain' });
+            res.write('Internal Server Error');
+        } else {
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.write(data);
+        }
+        res.end();
+    });
+}
+
+function handleGetProdutoImage(req, res) {
+    readFile('assets/produto.png', (err, data) => {
+        if (err) {
+            res.writeHead(500, { 'Content-Type': 'text/plain' });
+            res.write('Internal Server Error');
+        } else {
+            res.writeHead(200, { 'Content-Type': 'image/png' });
+            res.write(data);
+        }
+        res.end();
+    });
+}
+
 const server = createServer((req, res) => {
     const { url, method } = req;
 
-    if (url === '/api/produtos' && method === 'GET') {
-        res.writeHead(200, { 'Content-Type': 'application/json' });
-        res.write(lerDados());
-        res.end();
-    } else if (url === '/api/produtos' && method === 'POST') {
-        let body = '';
-        req.on('data', chunk => {
-            body += chunk;
-        });
-        req.on('end', () => {
-            const dados = JSON.parse(body);
-            const produtos = JSON.parse(lerDados());
-            produtos.push(dados);
-            salvarDados(produtos);
-            res.writeHead(201, { 'Content-Type': 'application/json' });
-            res.write(lerDados());
+    switch (url) {
+        case '/api/produtos':
+            if (method === 'GET') {
+                handleGetProdutos(req, res);
+            } else if (method === 'POST') {
+                handlePostProdutos(req, res);
+            } else {
+                res.writeHead(405, { 'Content-Type': 'text/plain' });
+                res.write('Method Not Allowed');
+                res.end();
+            }
+            break;
+        case '/':
+            if (method === 'GET') {
+                handleGetIndex(req, res);
+            } else {
+                res.writeHead(405, { 'Content-Type': 'text/plain' });
+                res.write('Method Not Allowed');
+                res.end();
+            }
+            break;
+        case '/assets/produto.png':
+            if (method === 'GET') {
+                handleGetProdutoImage(req, res);
+            } else {
+                res.writeHead(405, { 'Content-Type': 'text/plain' });
+                res.write('Method Not Allowed');
+                res.end();
+            }
+            break;
+        default:
+            res.writeHead(404, { 'Content-Type': 'text/plain' });
+            res.write('Not Found');
             res.end();
-        });
-    } else if (url === '/' && method === 'GET') {
-        readFile('index.html', (err, data) => {
-            if (err) {
-                res.writeHead(500, { 'Content-Type': 'text/plain' });
-                res.write('Internal Server Error');
-                res.end();
-            } else {
-                res.writeHead(200, { 'Content-Type': 'text/html' });
-                res.write(data);
-                res.end();
-            }
-        }); 
-    } else if (url === '/assets/produto.png' && method === 'GET') {
-        readFile('assets/produto.png', (err, data) => {
-            if (err) {
-                res.writeHead(500, { 'Content-Type': 'text/plain' });
-                res.write('Internal Server Error');
-                res.end();
-            } else {
-                res.writeHead(200, { 'Content-Type': 'image/png' });
-                res.write(data);
-                res.end();
-            }
-        });
-     } else {
-        res.writeHead(404, { 'Content-Type': 'text/plain' });
-        res.write('Not Found');
-        res.end();
+            break;
     }
 });
 
